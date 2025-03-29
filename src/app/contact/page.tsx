cceed60f337dc76a31e0d03799c4b16af23e8bc9
Tanadon Hunsadee <tanadonhun.dev@gmail.com>
2025-03-29 23:34:41 +0700

    set Styling import font

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,9 @@
+export default function ContactPage() {
+  return (
+    <main>
+      <h2 className="font-k2d text-3xl text-teal-700 font-bold">ติดต่อเรา</h2>
+      <p className="text-4xl">สวัสดี Contact Page</p>
+      <p className="font-sarabun text-4xl">สวัสดี Contact Page</p>
+    </main>
+  );
+}
