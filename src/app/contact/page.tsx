124b08c99931f186ab2d222b71d1f13ec08430bb
Tanadon Hunsadee <tanadonhun.dev@gmail.com>
2025-03-30 20:49:17 +0700

    add npx shadcn@latest add button

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export default function ContactPage() {
   return (
     <main>
       <h2 className="font-k2d text-3xl text-teal-700 font-bold">ติดต่อเรา</h2>
       <p className="text-4xl">สวัสดี Contact Page</p>
       <p className="font-sarabun text-4xl">สวัสดี Contact Page</p>
+      <Button variant="outline">Click Me</Button>
     </main>
   );
 }
