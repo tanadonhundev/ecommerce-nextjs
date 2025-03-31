6d94dc7ff374f74e6461f7c79b748549dea4fa89
Tanadon Hunsadee <tanadonhun.dev@gmail.com>
2025-03-31 21:46:45 +0700

    add singup Form and button singup

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -58,7 +58,7 @@ export function LoginForm({
             </div>
             <div className="mt-4 text-center text-sm">
               Don&apos;t have an account?{" "}
-              <a href="#" className="underline underline-offset-4">
+              <a href="/singup" className="underline underline-offset-4">
                 Sign up
               </a>
             </div>
---
+++ src/components/singup-form.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+const SingUpForm = () => {
+  return (
+    <div>singup-form</div>
+  )
+}
+
+export default SingUpForm
