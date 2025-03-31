984fb5a215945cde8bef3bf870157afc726d9831
Tanadon Hunsadee <tanadonhun.dev@gmail.com>
2025-03-31 20:46:28 +0700

    config better auth

---
+++ src/lib/auth.ts
@@ -0,0 +1,11 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import conn from "@/db"; // your drizzle instance
+
+const db = await conn;
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "mysql", // or "pg", "sqlite"
+  }),
+});
