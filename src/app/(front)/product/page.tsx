d603b6209e0075b7a487218574f2d74144155da0
Tanadon Hunsadee <tanadonhun.dev@gmail.com>
2025-04-02 21:38:30 +0700

    add AppProductDisplay.tsx

--- src/app/(front)/product/page.tsx
+++ src/app/(front)/product/page.tsx
@@ -1,7 +1,11 @@
-export default function ProductPage() {
+import AppProductDisplay from "@/components/app/AppProductDisplay";
+import { getProductService } from "@/services/product-service";
+
+export default async function ProductPage() {
+  const products = await getProductService();
   return (
     <div className="mx-auto max-w-4xl pt-16 pb-10">
-      <p>product page</p>
+      <AppProductDisplay products={products} />
     </div>
   );
 }
